f8ccc2fb49f2bf43d12d65dbc1d38b99c58f1590
Samuel Argueta <arg211024@uvg.edu.gt>
2024-07-11 18:27:45 -0600

    front

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DahsboardPage() {
+  return (
+    <div>DahsboardPage</div>
+  )
+}
+
+export default DahsboardPage
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import AppBar from "@/components/AppBar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main >
+      <p>Se esta desarrollando</p>
+    </main>
+  );
+}
